214e73dd1cdff66b19fdc4e951542ff306d07966
StevenLe456 <stevenhle456@outlook.com>
2024-10-21 13:42:32 -0500

    Preprocessed data

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,7 @@
+import networkx as nx
 import os
 import pandas as pd
+import pickle
 
 df = pd.read_csv("phenotype.csv")
 
@@ -8,11 +10,23 @@ with open("AAL.txt") as f:
 
 brain_regions = {i // 2 + 1: s for i, s in enumerate(raw_str.split()) if i % 2 == 1}
 
-list_of_files = []
+subj = {}
 
 for root, dirs, files in os.walk("ABIDEII-connectomes"):
     for f in files:
         filepath = os.path.join(root, f)
         if filepath.find("AAL_space-") + 1:
-            list_of_files.append(filepath)
+            subj[int(filepath[filepath.index("sub-") + 4 : filepath.index("sub-") + 9])] = filepath
 
+subj_dict = {}
+
+for subid, s in subj.items():
+    temp = {}
+    g = nx.to_dict_of_dicts(nx.relabel_nodes(nx.read_weighted_edgelist(s, create_using=nx.DiGraph), brain_regions))
+    p = df.loc[df["SUB_ID"] == subid].to_dict()
+    temp["graph"] = g
+    temp["phenotype"] = p
+    subj_dict[subid] = temp
+
+with open("subjects.pickle", "wb") as p:
+    pickle.dump(subj_dict, p)
